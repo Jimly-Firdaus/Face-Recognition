3e948ad2fa57ffde835241ab9ea99bf1f757a77d
MarcelRyan <16521343@std.stei.itb.ac.id>
2022-11-06 16:11:27 +0700

    nambah def mean + def selisih

--- src/eigenface.py
+++ src/eigenface.py
@@ -1,8 +1,17 @@
-def mean():
-    pass
+import numpy as np
 
-def selisih():
-    pass
+def mean(matrix):
+    mean_matrix = [0 for i in range(65536)]
+    for i in range(len(matrix)):
+        temp = 1/len(matrix) * matrix[i]
+        mean_matrix = np.add(temp, mean)
+    return mean_matrix
+
+def selisih(mean, matrix):
+    matrix_selisih = [[0 for i in range(65536)] for i in range(len(matrix))]
+    for i in range(len(matrix)):
+        matrix_selisih[i] = np.subtract(matrix[i], mean)
+    return matrix_selisih
 
 def covariant():
     pass
